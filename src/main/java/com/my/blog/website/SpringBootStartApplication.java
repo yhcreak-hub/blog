8f0add49be4295a85d0e0068bef428ea6f821ea3
yhcreak <yongheng.chen1@geely.com>
2019-12-25 17:04:23 +0800

    SpringBootStartApplication

--- src/main/java/com/my/blog/website/SpringBootStartApplication.java
+++ src/main/java/com/my/blog/website/SpringBootStartApplication.java
@@ -1,6 +1,5 @@
 package com.my.blog.website;
 
-import javafx.application.Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -8,6 +7,6 @@ public class SpringBootStartApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里一定要指向原先用main方法执行的Application启动类
-        return builder.sources(Application.class);
+        return builder.sources(CoreApplication.class);
     }
 }
